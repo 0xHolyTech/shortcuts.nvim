1b1d2b8e5dc9dc3ee308d358d997b0261fd1c725
Holy Tech <holytech.twitch@gmail.com>
2025-05-31 16:20:03 -0600

    Function layout

--- lua/shortcuts.lua
+++ lua/shortcuts.lua
@@ -2,5 +2,22 @@ local M = {
     shortcuts = {}
 }
 
+function M.get_project_shortcuts(projects)
+end
+
+function M.get_current_project()
+end
+
+function M.add_shortcut(shortcut)
+end
+
+function M.setup()
+    local project = M.get_current_project()
+    M.shortcuts = M.get_project_shortcuts(project)
+    for shortcut in M.shortcuts do
+        M.add_shortcut(shortcut)
+    end
+end
+
 return M
 
--- plugin/shortcuts.lua
+++
@@ -1,2 +0,0 @@
-local shortcuts = require('shortcuts')
-
