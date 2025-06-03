00fb652a21bcafe1e314f3ef735d9f4fabce91ed
Holy Tech <holytech.twitch@gmail.com>
2025-06-03 16:21:46 -0600

    Get git project directory

--- lua/shortcuts.lua
+++ lua/shortcuts.lua
@@ -3,9 +3,23 @@ local M = {
 }
 
 function M.get_project_shortcuts(projects)
+    local resp vim.system({'bash', '-c [ ! -e ~/.local/share/nvim/shortcuts/1.json]'})
+    if resp.code ~= 0 then
+        vim.system('touch', '~/.local/share/nvim/shortcuts/1.json')
+    end
+    return resp.code
+    -- file_exists
+    -- create if not
+    -- read_file
 end
 
 function M.get_current_project()
+    local cwd = vim.loop.cwd()
+    local root = vim.fn.system("git rev-parse --show-toplevel")
+    if vim.v.shell_error == 0 and root ~= nil then
+        return string.gsub(root, "\n", "")
+    end
+    return cwd
 end
 
 function M.add_shortcut(shortcut)
@@ -18,6 +32,8 @@ function M.setup()
         M.add_shortcut(shortcut)
     end
 end
+
+print(M.get_current_project())
 
 return M
 
