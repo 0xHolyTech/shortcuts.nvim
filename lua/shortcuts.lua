c18137dfbaca1729a85fbb3e4c565ed6798d2e9b
Holy Tech <holytech.twitch@gmail.com>
2025-06-08 17:50:29 -0600

    Fix system calling nvim and template out functions

--- lua/shortcuts.lua
+++ lua/shortcuts.lua
@@ -9,13 +9,13 @@ end
 
 function M.get_project_shortcuts(projects)
     -- create if not
-    vim.system({'mkdir -p ~/.local/share/nvim/shortcuts'}):wait()
-    vim.system({'touch ~/.local/share/nvim/shortcuts/1.json ]'}):wait()
+    vim.fn.system('mkdir -p ~/.local/share/nvim/shortcuts')
+    vim.fn.system('touch ~/.local/share/nvim/shortcuts/1.json ]')
     -- file_valid_json or empty file
-    file_manager.read_file('~/.local/share/nvim/shortcuts/1.json')
-    if file_manager.is_empty() then
-        file_manager.fill_template()
-    elseif file_manager.invalid_json() then
+    f_manager.read_file('~/.local/share/nvim/shortcuts/1.json')
+    if f_manager.is_empty() then
+        f_manager.fill_template()
+    elseif f_manager.invalid_json() then
         vim.api.nvim_err_writeln('ERRORED')
     end
     -- read_file
@@ -23,7 +23,7 @@ end
 
 function M.get_current_project()
     local cwd = vim.loop.cwd()
-    local root = vim.fn.system("git rev-parse --show-toplevel")
+    local root = vim.fn.system('git rev-parse --show-toplevel')
     if vim.v.shell_error == 0 and root ~= nil then
         return string.gsub(root, "\n", "")
     end
