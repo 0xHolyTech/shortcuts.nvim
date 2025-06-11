9e8b25abac5ab3bb5900025c0580b0baa66032f8
Holy Tech <holytech.twitch@gmail.com>
2025-06-10 19:03:36 -0600

    File path cleanup

--- lua/shortcuts/file_utils.lua
+++ lua/shortcuts/file_utils.lua
@@ -2,9 +2,16 @@ local json = require('shortcuts.utils')
 
 local FileManager = {}
 
+local plugin_path = '~/.local/share/nvim/shortcuts'
+
+function FileManager.touch(file)
+    vim.fn.system('mkdir -p ' .. plugin_path)
+    vim.fn.system('touch '.. plugin_path .. file)
+end
+
 function FileManager.read_file(file)
     local lines = ''
-    for line in io.lines(file) do
+    for line in io.lines(plugin_path .. file) do
       lines = lines .. '\n' .. line
     end
     return lines
