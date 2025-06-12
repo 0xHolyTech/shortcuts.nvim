4165ec73fd34e0b55e4eabbbcb123f28f8301b4e
Holy Tech <holytech.twitch@gmail.com>
2025-06-12 12:19:06 -0600

    Move utils and fix paths

--- lua/shortcuts/utils/files.lua
+++ lua/shortcuts/utils/files.lua
@@ -1,8 +1,8 @@
-local json = require('shortcuts.utils')
+local json = require('shortcuts.utils.json')
 
 local FileManager = {}
 
-local plugin_path = '~/.local/share/nvim/shortcuts'
+local plugin_path = vim.fn.expand('$HOME/.local/share/nvim/shortcuts/')
 
 function FileManager.touch(file)
     vim.fn.system('mkdir -p ' .. plugin_path)
@@ -19,14 +19,15 @@ end
 
 function FileManager.is_empty(file)
     local content = FileManager.read_file(file)
-    return content.size() == 0
+    return content == ''
 end
 
-function FileManager.is_valid_json(file)
+function FileManager.is_invalid_json(file)
     local content = FileManager.read_file(file)
     local should_be_json = json.decode(content)
     print(should_be_json)
     print(type(should_be_json))
+    return true
 end
 
 return FileManager
