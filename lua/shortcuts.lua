1c66a5859e92ccbbb86188f0af04cfc951922c2f
Holy Tech <holytech.twitch@gmail.com>
2025-06-17 01:23:32 -0600

    Fix shortcut multi shortcut structure

--- lua/shortcuts.lua
+++ lua/shortcuts.lua
@@ -3,35 +3,34 @@ local f_manager = require('shortcuts.utils.files')
 local M = {
     shortcuts = {
         n = {
-            keybind = 'p',
-            command = 'echo "WORKS"',
+            p = {
+                command = 'echo "WORKS"',
+                is_bash = true,
+            },
         }
     },
     prefix = '<leader>a'
 }
 
-function M.is_invalid_shortcut(mode, shortcut)
+function M.is_invalid_shortcut(mode, keybind, shortcut)
     if shortcut == nil then
         return true
     end
-    if type(shortcut.keybind) ~= 'string' then
+    if type(keybind) ~= 'string' then
         return true
     end
     return false
 end
 
 function M.get_project_shortcuts(project)
-    -- create if not
     local fn = project .. '.json'
     f_manager.touch(fn)
-    -- file_valid_json or empty file
     if f_manager.is_empty(fn) then
         f_manager.fill_template(fn, M.shortcuts)
     end
     if f_manager.is_invalid_json(fn) then
         return M.shortcuts
     end
-    -- read_file
     return f_manager.get_json(fn)
 end
 
@@ -46,18 +45,24 @@ function M.get_current_project()
     return string.gsub(cwd, '/', '.')
 end
 
-function M.add_shortcut(mode, shortcut)
-    if M.is_invalid_shortcut(mode, shortcut) then
+function M.add_shortcut(mode, keybind, shortcut)
+    if M.is_invalid_shortcut(mode, keybind, shortcut) then
         vim.api.nvim_err_writeln('INVALID SHORTCUT: ' .. vim.inspect(shortcut))
     end
-    vim.keymap.set(mode, M.prefix .. shortcut.keybind, '<Cmd>' .. shortcut.command .. '<CR>')
+    if shortcut.is_bash then
+        vim.keymap.set(mode, M.prefix .. keybind, '<Cmd>' .. shortcut.command .. '<CR>')
+    else
+        vim.keymap.set(mode, M.prefix .. keybind, shortcut.command)
+    end
 end
 
 function M.setup()
     local project = M.get_current_project()
     M.shortcuts = M.get_project_shortcuts(project)
     for mode, shortcut in pairs(M.shortcuts) do
-        M.add_shortcut(mode, shortcut)
+        for keybind, command in pairs(shortcut) do
+            M.add_shortcut(mode, keybind, command)
+        end
     end
 end
 
