237da1853467f18718697e7f871ed8c914d46d8b
Holy Tech <holytech.twitch@gmail.com>
2025-06-22 21:49:38 -0600

    Plan for file name passing

--- lua/shortcuts/ui.lua
+++ lua/shortcuts/ui.lua
@@ -5,28 +5,30 @@ M = {
     width = 50,
     borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
     bufnr = vim.api.nvim_create_buf(false, true),
+    project = ''
 }
 
-function M.setup()
+function M.setup(project)
+    M.project = project
     vim.api.nvim_create_autocmd('BufLeave', {
         buffer = M.bufnr,
         callback = function()
             -- This should save to file
-            print('YAYAYAYA')
+            -- :write M.project
         end
     })
     vim.api.nvim_create_autocmd('BufEnter', {
         buffer = M.bufnr,
         callback = function()
             -- This should load from file
-            print('BABABABA')
+            -- :edit M.project
         end
     })
 end
 
 function M.ShowMenu()
     M.Win_id = popup.create(M.bufnr, {
-        title = "MyProjects",
+        title = M.project,
         highlight = "MyProjectWindow",
         line = math.floor(((vim.o.lines - M.height) / 2) - 1),
         col = math.floor((vim.o.columns - M.width) / 2),
@@ -41,7 +43,5 @@ end
 function M.HideMenu()
     vim.api.nvim_win_hide(M.Win_id)
 end
-
-M.setup()
 
 return M
